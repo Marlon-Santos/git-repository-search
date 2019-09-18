545d7c2d1d2afc44f54d826ead6848603b38ebbf
git <marlonergondossantos@gmail.com>
2019-09-18 11:11:53 -0300

    Global Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import GlobalStyle from "./styles/globalStyle";
+
 import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
       <>
+        <GlobalStyle />
         <h1>funcionando</h1>
       </>
     );

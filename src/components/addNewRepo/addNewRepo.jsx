d13dd0e612b9f9fd2e365e7d4a320eb4a9bb1a96
git <marlonergondossantos@gmail.com>
2019-09-19 13:32:56 -0300

    test Api

--- src/components/addNewRepo/addNewRepo.jsx
+++ src/components/addNewRepo/addNewRepo.jsx
@@ -2,7 +2,36 @@ import React, { Component } from "react";
 import { Container, DivInput } from "../../styles/addNewRepo/addNewRepoStyle";
 import { DivTitle } from "../../styles/addNewRepo/addNewRepoStyle";
 import GitLogo from "../../assets/gitLogo.png";
+import api from "../../server/gitApi";
 class AddNewRepo extends Component {
+  state = {
+    newRepo: "",
+    repo: []
+  };
+  adicionarRepo = e => {
+    e.preventDefault();
+    this.setState({ newRepo: e.target[0].value });
+  };
+  componentDidUpdate(_, prevState) {
+    if (this.state.newRepo !== prevState.newRepo) {
+      const test = async () => {
+        try {
+          const ok = await api.get(this.state.newRepo);
+          return ok;
+        } catch (e) {
+          return e;
+        }
+      };
+      test().then(resolve => {
+        if (resolve.request.status === 200) {
+          this.setState({ repo: [...this.state.repo, resolve.data.name] });
+          this.setState({ newRepo: "" });
+        } else {
+        }
+      });
+    }
+  }
+
   render() {
     return (
       <>
@@ -11,9 +40,16 @@ class AddNewRepo extends Component {
             <img src={GitLogo} alt="git logo" />
             <h1>Repositórios:</h1>
           </DivTitle>
-          <DivInput>
-            <input type="text" />
-            <input type="button" value="+" />
+          {this.state.repo.map(item => {
+            return <h3>{item}</h3>;
+          })}
+          <DivInput
+            onSubmit={e => {
+              this.adicionarRepo(e);
+            }}
+          >
+            <input type="text" name="repo" placeholder="EX: user/repo" />
+            <input type="submit" value="+" />
           </DivInput>
         </Container>
       </>

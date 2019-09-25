5316896a73be83d368fd9fcdd7201656171cce48
git <marlonergondossantos@gmail.com>
2019-09-25 09:55:06 -0300

    react router

---
+++ src/pages/historicUser/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function historic() {
+  return <div>historico</div>;
+}
---
+++ src/pages/listRepo/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function listRepo() {
+  return <div>lista de repos</div>;
+}
